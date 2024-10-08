8f9ef5dda4540825ea265be55d70fc1f20968b67
DuttaAnirudh <worktodutta@gmail.com>
2024-10-08 21:42:08 +0530

    build a simple api; creating a website using html templates

--- 01-node-farm/index.js
+++ 01-node-farm/index.js
@@ -36,18 +36,85 @@ const url = require("url");
 /**************************************/
 /** SERVER **/
 
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+// Updating Template with data
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+
+  return output;
+};
+
 /** Creating a Server **/
-
 const server = http.createServer((req, res) => {
-  console.log(req.url);
+  const { query, pathname } = url.parse(req.url, true);
 
-  const pathName = req.url;
+  // Overview Page
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
 
-  if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the Overview");
-  } else if (pathName === "/product") {
-    res.end("This is the Product");
-  } else {
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+
+    const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+
+    res.end(output);
+  }
+  // Product Page
+  else if (pathname === "/product") {
+    console.log(query);
+
+    const product = dataObj[query.id];
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+  }
+  // API
+  else if (pathname === "/api") {
+    /* Reading data everytime we access the API */
+    // res.end("API");
+    // fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
+    //   const productData = JSON.parse(data);
+    //   console.log(productData);
+    //   res.writeHead(200, { "Content-type": "application/json" });
+    //   res.end(data);
+    // });
+
+    /* Reading data once and storing it in a variable */
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  }
+  // Not found
+  else {
     res.writeHead(404, {
       "Content-type": "text/html",
       "my-own-header": "hello-world", // custom header
