8f0732af7ace1cc23ab1ac0bd55d60dc1646cd91
DuttaAnirudh <worktodutta@gmail.com>
2024-10-09 18:41:52 +0530

    exporting our own modules and 3rd party modules; package versioning and updating

--- 01-node-farm/index.js
+++ 01-node-farm/index.js
@@ -1,6 +1,9 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const slugify = require("slugify");
+
+const replaceTemplate = require("./modules/replaceTemplate");
 
 // const hello = "Hello world";
 // console.log(hello);
@@ -51,26 +54,12 @@ const tempProduct = fs.readFileSync(
   "utf-8"
 );
 
+// TODO
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
-// Updating Template with data
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-
-  return output;
-};
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 /** Creating a Server **/
 const server = http.createServer((req, res) => {
@@ -79,7 +68,6 @@ const server = http.createServer((req, res) => {
   // Overview Page
   if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "Content-type": "text/html" });
-
     const cardsHtml = dataObj
       .map((el) => replaceTemplate(tempCard, el))
       .join("");
