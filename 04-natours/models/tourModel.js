09a65ec3f90878df4d3a3c0ad8543172bd828c73
DuttaAnirudh <worktodutta@gmail.com>
2024-11-10 21:41:13 +0530

    preventing duplicate reviews; finding tours within radius and calculating their distances

--- 04-natours/models/tourModel.js
+++ 04-natours/models/tourModel.js
@@ -38,6 +38,7 @@ const tourSchema = new mongoose.Schema(
       default: 4.5,
       min: [1, 'Rating must be above 1.0'], // only for Numbers & Dates
       max: [5, 'Rating must be below 5.0'], // only for Numbers & Dates
+      set: (val) => Math.round(val * 10) / 10,
     },
     ratingsQuantity: {
       type: Number,
@@ -136,6 +137,8 @@ tourSchema.index({
   slug: 1,
 });
 
+tourSchema.index({ startLocation: '2dsphere' });
+
 // VIRTUAL PROPERTIES
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
@@ -212,13 +215,13 @@ tourSchema.pre(/^find/, function (next) {
 
 /////////////////////////////////////////////////////////
 // AGGREGATION MIDDLEWARE
-tourSchema.pre('aggregate', function (next) {
-  // adding a new stage ('match') which removes all tours with "secrectTour" propert set to "true"
-  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-  // console.log(this.pipeline()); // "this" points to the current aggregation object
+// tourSchema.pre('aggregate', function (next) {
+//   // adding a new stage ('match') which removes all tours with "secrectTour" propert set to "true"
+//   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+//   // console.log(this.pipeline()); // "this" points to the current aggregation object
 
-  next();
-});
+//   next();
+// });
 
 // Creating a Model out of the Schema
 // always use Uppercase in model names
