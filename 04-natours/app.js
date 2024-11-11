a8fa3d2e8f08e6deaecf75f2d1ac4b33f58853c9
DuttaAnirudh <worktodutta@gmail.com>
2024-11-11 20:46:05 +0530

    setting up pug

--- 04-natours/app.js
+++ 04-natours/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
@@ -14,6 +15,13 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
+
+app.set('view-engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serving static files from a folder
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
 
 /* MIDDLEWARE */
 // SET Security HTTP Headers
@@ -57,9 +65,6 @@ app.use(
   }),
 );
 
-// Serving static files from a folder
-app.use(express.static(`${__dirname}/public`));
-
 // CUSTOM MIDDLEWARE
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -68,6 +73,13 @@ app.use((req, res, next) => {
 });
 
 /* MOUNTING THE ROUTER */
+app.get('/', (req, res) => {
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Jonas',
+  });
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
